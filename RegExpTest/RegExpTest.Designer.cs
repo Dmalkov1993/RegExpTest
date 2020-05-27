955b02b9573d7697bd45c9d380ff2b1b51194729
Dmalkov <Denis1993@yandex.ru>
2020-05-27 23:53:07 +0500

    First commit

---
+++ RegExpTest/RegExpTest.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace RegExpTest
+{
+    partial class RegExpTest
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonReplace = new System.Windows.Forms.RadioButton();
+            this.radioButtonIsMatch = new System.Windows.Forms.RadioButton();
+            this.checkBoxIgnoreCase = new System.Windows.Forms.CheckBox();
+            this.textBoxRegExp = new System.Windows.Forms.TextBox();
+            this.textBoxInputText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxResultText = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelTextError = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labelUpperTextError = new System.Windows.Forms.Label();
+            this.labelForReplace = new System.Windows.Forms.Label();
+            this.textBoxReplacementText = new System.Windows.Forms.TextBox();
+            this.checkBoxInputTextAsOneString = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButtonReplace
+            // 
+            this.radioButtonReplace.AutoSize = true;
+            this.radioButtonReplace.Location = new System.Drawing.Point(76, 11);
+            this.radioButtonReplace.Name = "radioButtonReplace";
+            this.radioButtonReplace.Size = new System.Drawing.Size(65, 17);
+            this.radioButtonReplace.TabIndex = 0;
+            this.radioButtonReplace.Text = "Replace";
+            this.radioButtonReplace.UseVisualStyleBackColor = true;
+            this.radioButtonReplace.CheckedChanged += new System.EventHandler(this.RegExpMainEvent);
+            // 
+            // radioButtonIsMatch
+            // 
+            this.radioButtonIsMatch.AutoSize = true;
+            this.radioButtonIsMatch.Checked = true;
+            this.radioButtonIsMatch.Location = new System.Drawing.Point(7, 11);
+            this.radioButtonIsMatch.Name = "radioButtonIsMatch";
+            this.radioButtonIsMatch.Size = new System.Drawing.Size(63, 17);
+            this.radioButtonIsMatch.TabIndex = 1;
+            this.radioButtonIsMatch.TabStop = true;
+            this.radioButtonIsMatch.Text = "IsMatch";
+            this.radioButtonIsMatch.UseVisualStyleBackColor = true;
+            this.radioButtonIsMatch.CheckedChanged += new System.EventHandler(this.RegExpMainEvent);
+            // 
+            // checkBoxIgnoreCase
+            // 
+            this.checkBoxIgnoreCase.AutoSize = true;
+            this.checkBoxIgnoreCase.Location = new System.Drawing.Point(329, 11);
+            this.checkBoxIgnoreCase.Name = "checkBoxIgnoreCase";
+            this.checkBoxIgnoreCase.Size = new System.Drawing.Size(124, 17);
+            this.checkBoxIgnoreCase.TabIndex = 2;
+            this.checkBoxIgnoreCase.Text = "Без учёта регистра";
+            this.checkBoxIgnoreCase.UseVisualStyleBackColor = true;
+            this.checkBoxIgnoreCase.CheckedChanged += new System.EventHandler(this.RegExpMainEvent);
+            // 
+            // textBoxRegExp
+            // 
+            this.textBoxRegExp.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxRegExp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxRegExp.Location = new System.Drawing.Point(94, 40);
+            this.textBoxRegExp.Name = "textBoxRegExp";
+            this.textBoxRegExp.Size = new System.Drawing.Size(469, 22);
+            this.textBoxRegExp.TabIndex = 3;
+            this.textBoxRegExp.TextChanged += new System.EventHandler(this.RegExpMainEvent);
+            // 
+            // textBoxInputText
+            // 
+            this.textBoxInputText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInputText.Location = new System.Drawing.Point(94, 88);
+            this.textBoxInputText.Multiline = true;
+            this.textBoxInputText.Name = "textBoxInputText";
+            this.textBoxInputText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxInputText.Size = new System.Drawing.Size(470, 86);
+            this.textBoxInputText.TabIndex = 4;
+            this.textBoxInputText.TextChanged += new System.EventHandler(this.RegExpMainEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Рег. выражение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Входной текст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Результат";
+            // 
+            // textBoxResultText
+            // 
+            this.textBoxResultText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxResultText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxResultText.Location = new System.Drawing.Point(94, 178);
+            this.textBoxResultText.Multiline = true;
+            this.textBoxResultText.Name = "textBoxResultText";
+            this.textBoxResultText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResultText.Size = new System.Drawing.Size(470, 86);
+            this.textBoxResultText.TabIndex = 4;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelTextError});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 274);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(575, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelTextError
+            // 
+            this.toolStripStatusLabelTextError.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripStatusLabelTextError.Name = "toolStripStatusLabelTextError";
+            this.toolStripStatusLabelTextError.Size = new System.Drawing.Size(156, 17);
+            this.toolStripStatusLabelTextError.Text = "toolStripStatusLabelTextError";
+            // 
+            // labelUpperTextError
+            // 
+            this.labelUpperTextError.AutoSize = true;
+            this.labelUpperTextError.ForeColor = System.Drawing.Color.Red;
+            this.labelUpperTextError.Location = new System.Drawing.Point(5, 257);
+            this.labelUpperTextError.Name = "labelUpperTextError";
+            this.labelUpperTextError.Size = new System.Drawing.Size(64, 13);
+            this.labelUpperTextError.TabIndex = 9;
+            this.labelUpperTextError.Text = "UpTextError";
+            // 
+            // labelForReplace
+            // 
+            this.labelForReplace.AutoSize = true;
+            this.labelForReplace.Location = new System.Drawing.Point(4, 66);
+            this.labelForReplace.Name = "labelForReplace";
+            this.labelForReplace.Size = new System.Drawing.Size(75, 13);
+            this.labelForReplace.TabIndex = 10;
+            this.labelForReplace.Text = "Заменить на:";
+            // 
+            // textBoxReplacementText
+            // 
+            this.textBoxReplacementText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxReplacementText.Enabled = false;
+            this.textBoxReplacementText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxReplacementText.Location = new System.Drawing.Point(94, 64);
+            this.textBoxReplacementText.Name = "textBoxReplacementText";
+            this.textBoxReplacementText.Size = new System.Drawing.Size(470, 22);
+            this.textBoxReplacementText.TabIndex = 11;
+            this.textBoxReplacementText.TextChanged += new System.EventHandler(this.RegExpMainEvent);
+            // 
+            // checkBoxInputTextAsOneString
+            // 
+            this.checkBoxInputTextAsOneString.AutoSize = true;
+            this.checkBoxInputTextAsOneString.Location = new System.Drawing.Point(147, 11);
+            this.checkBoxInputTextAsOneString.Name = "checkBoxInputTextAsOneString";
+            this.checkBoxInputTextAsOneString.Size = new System.Drawing.Size(176, 17);
+            this.checkBoxInputTextAsOneString.TabIndex = 12;
+            this.checkBoxInputTextAsOneString.Text = "Входной текст одной строкой";
+            this.checkBoxInputTextAsOneString.UseVisualStyleBackColor = true;
+            this.checkBoxInputTextAsOneString.CheckedChanged += new System.EventHandler(this.RegExpMainEvent);
+            // 
+            // RegExpTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 296);
+            this.Controls.Add(this.checkBoxInputTextAsOneString);
+            this.Controls.Add(this.textBoxReplacementText);
+            this.Controls.Add(this.labelForReplace);
+            this.Controls.Add(this.labelUpperTextError);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxResultText);
+            this.Controls.Add(this.textBoxInputText);
+            this.Controls.Add(this.textBoxRegExp);
+            this.Controls.Add(this.checkBoxIgnoreCase);
+            this.Controls.Add(this.radioButtonIsMatch);
+            this.Controls.Add(this.radioButtonReplace);
+            this.MinimumSize = new System.Drawing.Size(546, 285);
+            this.Name = "RegExpTest";
+            this.Text = "RegExpTest";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.RegExpTest_FormClosed);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonReplace;
+        private System.Windows.Forms.RadioButton radioButtonIsMatch;
+        private System.Windows.Forms.CheckBox checkBoxIgnoreCase;
+        private System.Windows.Forms.TextBox textBoxRegExp;
+        private System.Windows.Forms.TextBox textBoxInputText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxResultText;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelTextError;
+        public System.Windows.Forms.Label labelForReplace;
+        public System.Windows.Forms.TextBox textBoxReplacementText;
+        public System.Windows.Forms.Label labelUpperTextError;
+        private System.Windows.Forms.CheckBox checkBoxInputTextAsOneString;
+    }
+}
+
---
+++ RegExpTest/RegExpTest.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FileSettings;
+
+
+namespace RegExpTest
+{
+    public partial class RegExpTest : Form
+    {
+        public RegExpTest()
+        {
+            InitializeComponent();
+
+            #region Выпилено в пространство имён FileSettings в метод PositionXY.Start(this)
+            /*
+            var appSettings = ConfigurationManager.AppSettings;
+            //Считываем координаты экрана последнего запуска приложения
+            string StartPositionX = appSettings["StartPositionX"] ?? "Not Found";
+            string StartPositionY = appSettings["StartPositionY"] ?? "Not Found";
+            try
+            {
+                //Говорим что сами Вручную укажем координаты и не надо нам ничего
+                this.StartPosition = FormStartPosition.Manual;
+                //Получаем интовые значения
+                int IntStartPositionX = Convert.ToInt16(StartPositionX);
+                int IntStartPositionY = Convert.ToInt16(StartPositionY);
+                if (IntStartPositionX <= 1024 && IntStartPositionY <= 768)
+                {
+                    //применяем считанные координаты
+                    this.SetDesktopLocation(IntStartPositionX, IntStartPositionY);
+                }
+                else
+                {
+                    //MessageBox.Show("больше чем 1024 768");
+                    throw new Exception("Не получится отобразить используя старые координаты");
+                }
+            }
+            /*Exception возникнет, когда:
+                1) в файле нет подобных записей, выдаст Not found -> не сможет преобр. в Int;
+                2) в файле указаны тексты, вместо цыфорок;
+                3) указанные координаты выходят за пределы 1024 768 (throw new Exception);
+             * во всех этих случаях - рисуем форму в центре экрана
+             */
+            /*
+            catch
+            {
+                //MessageBox.Show("либо нету, либо написаны не Инт значения, либо \"больше чем 1024 768\"");
+                //рисуем по центру
+                this.CenterToScreen();
+            }
+            */
+            #endregion
+
+            //Стартуем текущую форму со значениями Х Y из конф. файла
+            PositionXY.Start(this);
+
+            //стираем лейбл, говорящий об ошибках
+            toolStripStatusLabelTextError.Text = labelUpperTextError.Text = null;
+
+            //отключаем текст бокс для текста замены:
+            labelForReplace.ForeColor = Color.Gray;
+            //labelForReplace.Refresh();
+            textBoxReplacementText.Text = "Текст замены. Используется только для метода замены (replace)";
+            textBoxReplacementText.Enabled = false;
+        }
+
+        enum RegExpMethod
+        {
+            IsMatch,
+            Replace
+        }
+        //По умолчанию при старте программы чекнут Replace
+        RegExpMethod ChoisedRegExpMethod = RegExpMethod.IsMatch;
+
+        private void ControlsForReplace_Disabled()
+        {
+
+            //отключаем текст бокс для текста замены:
+            labelForReplace.ForeColor = Color.Gray;
+            textBoxReplacementText.Text = "Текст замены. Используется только для метода замены (replace)";
+            textBoxReplacementText.Enabled = false;
+        }
+
+        private void ControlsForReplace_Enabled()
+        {
+            //отключаем текст бокс для текста замены:
+            labelForReplace.ForeColor = Color.Black;
+            //Если текст стандартный, то убираем его
+            if (textBoxReplacementText.Text == "Текст замены. Используется только для метода замены (replace)")
+            {
+                textBoxReplacementText.Text = null;
+            }
+            textBoxReplacementText.Enabled = true;
+        }
+
+        #region Эти методы уже не нужны, поскольку у всех контролов указан один единственный метод RegExpMainEvent
+        //Позволит моментально находить результат рег выраж. при смене метода
+        /*
+        private void RegExpMethodChanged(object sender, EventArgs e)
+        {
+            //Если поменяли метод, то надо стереть результат
+            textBoxResultText.Text = null;
+
+            //Выполняем основной метод 
+            RegExpOrInputTextChanged(sender, e);
+        }
+         */
+        /*
+        private RegExpMethod GetRegExpMethod()
+        {
+
+            return ChoisedRegExpMethod;
+        }
+         */
+        #endregion
+
+        private void RegExpMainEvent(object sender, EventArgs e)
+        {
+            //Стираем предыдущий результат
+            textBoxResultText.Text = null;
+
+            //Очищаем лейбл говорящий об ошибках
+            toolStripStatusLabelTextError.Text = labelUpperTextError.Text = null;
+
+            //чекаем какая радиокнопка выбрана и запоминаем результат
+            if (radioButtonIsMatch.Checked)
+            {
+                //запоминаем выбранный метод
+                ChoisedRegExpMethod = RegExpMethod.IsMatch;
+            }
+
+            if (radioButtonReplace.Checked)
+            {
+                //Включаем поле для Replace
+                ControlsForReplace_Enabled();
+
+                //запоминаем выбранный метод
+                ChoisedRegExpMethod = RegExpMethod.Replace;
+                //MessageBox.Show(radioButtonReplace.Text);
+            }
+            else
+            {
+                //Отключаем поле для Replace, так как выбран метод, НЕ Replace
+                ControlsForReplace_Disabled();
+            }
+
+
+            //Проверяем, установлен ли флаг использования рег. выраж. без учёта регистра
+            //Объявим переменную опций для регулярки
+            RegexOptions options = RegexOptions.None;
+            if (checkBoxIgnoreCase.Checked)
+            {
+                //Если да, то добавляем данную опцию
+                options = RegexOptions.IgnoreCase;
+            }
+
+
+            //MessageBox.Show("текст изменён");
+
+
+            switch (ChoisedRegExpMethod)
+            {
+                case RegExpMethod.IsMatch:
+
+                    try
+                    {
+                        Regex rgx = new Regex(textBoxRegExp.Text, options);
+
+                        //Если стоит флаг "всё поле одной строкой", то убираем переносы строк
+                        if (checkBoxInputTextAsOneString.Checked)
+                        {
+                            textBoxResultText.Text = rgx.IsMatch(textBoxInputText.Text.Replace(Environment.NewLine, " ")).ToString();
+                        }
+                        else
+                        {
+                            //Не удаляя переносы строк
+                            textBoxResultText.Text = rgx.IsMatch(textBoxInputText.Text).ToString();
+                        }
+                        
+                        //MessageBox.Show("IsMatch");
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show("Кривой Рег експ");
+                        labelUpperTextError.Text = "Ошибка:";
+                        toolStripStatusLabelTextError.Text = ex.Message;
+
+                    }
+                    break;
+
+                case RegExpMethod.Replace:
+                    try
+                    {
+                        Regex rgx = new Regex(textBoxRegExp.Text, options);
+
+                        //1000 - Указали что надо делать замену в 1000 строках
+                        //Если стоит флаг "всё поле одной строкой", то убираем переносы строк
+                        if (checkBoxInputTextAsOneString.Checked)
+                        {
+                            textBoxResultText.Text = rgx.Replace(textBoxInputText.Text.Replace(Environment.NewLine, " "), textBoxReplacementText.Text, 1000);
+                        }
+                        else
+                        {
+                            //Не удаляя переносы строк
+                            textBoxResultText.Text = rgx.Replace(textBoxInputText.Text, textBoxReplacementText.Text, 1000);
+                        }
+
+                        
+
+                        //И пройдёмся по всем строкам
+                        /*
+                        foreach (string SingleLine in textBoxInputText.Lines) 
+                        {
+                            textBoxResultText.Text = textBoxResultText.Text + rgx.Replace(SingleLine, textBoxReplacementText.Text,1000);
+                        }
+                         */
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show("Кривой Рег експ");
+                        labelUpperTextError.Text = "Ошибка:";
+                        toolStripStatusLabelTextError.Text = ex.Message;
+                    }
+                    break;
+            }
+        }
+
+        private void RegExpTest_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Запоминаем координаты запуска, записав их в конф. файл
+            FileSettings.ChangeSettings.AddSetting("StartPositionX", this.Location.X.ToString());
+            FileSettings.ChangeSettings.AddSetting("StartPositionY", this.Location.Y.ToString());
+        }
+    }
+}
